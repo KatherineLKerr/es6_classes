ab9f323fb653dc9d4483dc3f52980d49884afc7a
Katherine Kerr <katherinekerr@protonmail.com>
2019-08-11 10:56:04 +0100

    changed classes, constructors and prototype methods

---
+++ client/src/helpers/pub_sub.js
@@ -0,0 +1,18 @@
+class PubSub = {
+
+  constructor() {
+  }
+
+  publish(channel, payload) {
+    var event = new CustomEvent(channel, {
+      detail: payload
+  });
+    document.dispatchEvent(event);
+  },
+
+  subscribe(channel, callback) {
+    document.addEventListener(channel, callback);
+  }
+};
+
+module.exports = PubSub;
